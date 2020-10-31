85d02f68426a63585cc331647e1b7eba7d741917
magneto0124 <1519095211@qq.com>
2020-10-31 17:31:30 +0800

    Testing testing

---
+++ Project.java
@@ -0,0 +1,18 @@
+public class Project{
+	public int a;
+	public int b;
+	
+	public Project() {
+		
+	}
+	
+	public Project(int a,int b) {
+		this.a=a;
+		this.b=b;
+	}
+	
+	public void output() {
+		System.out.println(a"+"b);
+	}
+	
+}
